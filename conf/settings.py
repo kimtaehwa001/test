d1ca748a24aa5b374d49c9f51a432d85b26b5a2c
kimtaehwa001 <xoghk2354@gmail.com>
2025-12-28 15:40:41 +0900

    222

--- conf/settings.py
+++ conf/settings.py
@@ -133,6 +133,7 @@ if IF_S3:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'ap-northeast-2'
+    AWS_QUERYSTRING_AUTH = False
 
     # S3 정적파일 엔진 설정
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
